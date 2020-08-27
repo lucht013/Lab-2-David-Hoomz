5156da39cb3bbf86f44fc7fc1c177e19b8c15eda
David <lucht013@morris.umn.edu>
2020-08-27 17:31:32 -0500

    End of class progress

--- src/TestInteger.java
+++ src/TestInteger.java
@@ -1,2 +1,21 @@
-public interface TestInteger {
+public class TestInteger implements Comparable<TestInteger> {
+    int value = 0;
+    static long counter = 0;
+    public static void main(String args[]){
+        TestInteger n = new TestInteger();
+    }
+
+    @Override
+    public int compareTo(TestInteger o) {
+        counter++;
+        if(value > o.value) {
+            return 1;
+        }else if(value < o.value){
+            return -1;
+        }else if(value == o.value){
+            return 0;
+        }
+        return -999;
+
+    }
 }
