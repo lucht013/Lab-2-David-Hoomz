9a3512e8fd33ee971d3e2a533afb4ebedcd2a601
David <lucht013@morris.umn.edu>
2020-09-08 19:21:50 -0500

    Add tests and measurments

--- src/Lab2.java
+++ src/Lab2.java
@@ -4,10 +4,17 @@ import java.util.Random;
 
 public class Lab2 {
     public static void main(String[] args){
-        System.out.println("Comparing 10,000 random testInts from 1 to 1,000,000 5 times...");
-        normalTimSortComparison();
-
-
+       System.out.println("Comparing 10,000 random testInts from 1 to 1,000,000 5 times...");
+       normalTimSortComparison();
+       System.out.println(" ");
+       System.out.println("Comparing 10,000 ordered testInts from 1 to 1,000,000 5 times...");
+       increasingOrdinalTimSortComparison();
+       System.out.println(" ");
+       System.out.println("Comparing 10 sorted sequences of 1,000 ordered elements from 1 to 1,000,000 5 times...");
+       tenSortedTimSortComparison();
+       System.out.println(" ");
+       System.out.println("Comparing 100 sorted sequences of 100 ordered elements from 1 to 1,000,000 5 times...");
+       hundredSortedTimSortComparison();
     }
 
     public static void quicksort(TestInteger A[], int p, int r){
@@ -47,7 +54,7 @@ public class Lab2 {
     }
 
     public static void normalTimSortComparison(){
-        int length = 50;
+        int length = 10000;
         int bound = 1000000;
         int repititions = 5;
         Random rand = new Random();
@@ -65,7 +72,94 @@ public class Lab2 {
             start = System.currentTimeMillis();
             Arrays.sort(timTestArray);
             end = System.currentTimeMillis();
+            System.out.println("Trial " + j + " TimSort result: " + (end-start) + " ms with " + timTestArray[0].getComparisons() + " comparisons");
+            System.out.println();
+        }
+    }
+
+    public static void increasingOrdinalTimSortComparison(){
+        int length = 10000;
+        int bound = 1000000;
+        int repititions = 5;
+        Random rand = new Random();
+        for(int j = 1; j<=repititions; j++){
+            TestInteger testArray[] = new TestInteger[length];
+            int seed = rand.nextInt(bound-10001);
+            testArray[0] = new TestInteger();
+            testArray[0].value = seed;
+            for (int i = 1; i < testArray.length; i++) {
+                testArray[i] = new TestInteger();
+                testArray[i].value = testArray[i-1].value + 1;
+            }
+            TestInteger timTestArray[] = testArray;
+            long start = System.currentTimeMillis();
+            quicksort(testArray, 0, testArray.length - 1);
+            long end = System.currentTimeMillis();
+            System.out.println("Trial " + j + " quicksort result: " + (end-start) + "ms with " + testArray[0].getComparisons() + " comparisons");
+            start = System.currentTimeMillis();
+            Arrays.sort(timTestArray);
+            end = System.currentTimeMillis();
             System.out.println("Trial " + j + "TimSort result: " + (end-start) + " ms with " + timTestArray[0].getComparisons() + " comparisons");
+            System.out.println();
+        }
+    }
+
+    public static void tenSortedTimSortComparison(){
+        int length = 10000;
+        int bound = 1000000;
+        int repititions = 5;
+        Random rand = new Random();
+        for(int j = 1; j<=repititions; j++){
+            TestInteger testArray[] = new TestInteger[length];
+            int seed = rand.nextInt(bound-1001);
+            for (int i = 0; i <= 9; i++) {
+                seed = rand.nextInt(bound-1001);
+                testArray[i * 1000] = new TestInteger();
+                testArray[i * 1000].value = seed;
+                for(int k = 1; k < 1000; k++){
+                    testArray[i * 1000 + k] = new TestInteger();
+                    testArray[i * 1000 + k].value = testArray[i * 1000 + k - 1].value + 1;
+                }
+            }
+            TestInteger timTestArray[] = testArray;
+            long start = System.currentTimeMillis();
+            quicksort(testArray, 0, testArray.length - 1);
+            long end = System.currentTimeMillis();
+            System.out.println("Trial " + j + " quicksort result: " + (end-start) + "ms with " + testArray[0].getComparisons() + " comparisons");
+            start = System.currentTimeMillis();
+            Arrays.sort(timTestArray);
+            end = System.currentTimeMillis();
+            System.out.println("Trial " + j + " TimSort result: " + (end-start) + " ms with " + timTestArray[0].getComparisons() + " comparisons");
+            System.out.println();
+        }
+    }
+
+    public static void hundredSortedTimSortComparison(){
+        int length = 10000;
+        int bound = 1000000;
+        int repititions = 5;
+        Random rand = new Random();
+        for(int j = 1; j<=repititions; j++){
+            TestInteger testArray[] = new TestInteger[length];
+            int seed = rand.nextInt(bound-1001);
+            for (int i = 0; i <= 99; i++) {
+                seed = rand.nextInt(bound-101);
+                testArray[i * 100] = new TestInteger();
+                testArray[i * 100].value = seed;
+                for(int k = 1; k < 100; k++){
+                    testArray[i * 100 + k] = new TestInteger();
+                    testArray[i * 100 + k].value = testArray[i * 100 + k - 1].value + 1;
+                }
+            }
+            TestInteger timTestArray[] = testArray;
+            long start = System.currentTimeMillis();
+            quicksort(testArray, 0, testArray.length - 1);
+            long end = System.currentTimeMillis();
+            System.out.println("Trial " + j + " quicksort result: " + (end-start) + "ms with " + testArray[0].getComparisons() + " comparisons");
+            start = System.currentTimeMillis();
+            Arrays.sort(timTestArray);
+            end = System.currentTimeMillis();
+            System.out.println("Trial " + j + " TimSort result: " + (end-start) + " ms with " + timTestArray[0].getComparisons() + " comparisons");
             System.out.println();
         }
     }
